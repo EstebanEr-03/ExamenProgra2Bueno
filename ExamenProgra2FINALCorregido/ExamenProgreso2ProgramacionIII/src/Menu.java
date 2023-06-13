fb7b3a903127030b71825fc22632fc99f7a2a394
EstebanEr-03 <125702762+EstebanEr-03@users.noreply.github.com>
2023-06-13 15:45:33 -0500

    Add files via upload

---
+++ ExamenProgra2FINALCorregido/ExamenProgreso2ProgramacionIII/src/Menu.java
@@ -0,0 +1,231 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Menu {
+    List<Plato> platosMenu=new ArrayList<>();
+    List<Plato> arregloBurbuja=new ArrayList<>();
+    List<Plato> arregloInsercion=new ArrayList<>();
+
+
+    public Menu() {
+    }
+
+    public void addPlatos(Plato plato1){
+        platosMenu.add(plato1);
+        BurbujaA();
+        getPlatosMenu();
+    }
+    public void removePackage(List<Plato> listaRemover,String trackingNumberT){
+        if (searchByTrackingNameLineal(listaRemover,trackingNumberT)>=0){
+            platosMenu.remove(searchByTrackingNameLineal(listaRemover,trackingNumberT));
+        }
+    }
+    public int searchByTrackingNameLineal(List<Plato> listaBuscar, String nombrePlato) {
+        for (int j=0; j<listaBuscar.size(); j++){
+            if (listaBuscar!= null && listaBuscar.size()>=0 && listaBuscar.get(j).getNombrePlato().equals(nombrePlato)){
+                return j;
+            }
+        }
+        return -1;
+    }
+    public  Plato searchBinary(List<Plato> listaBuscarPorTrackingBinario, String targetTrackingNameT) {
+        int izquierda=0, derecha= listaBuscarPorTrackingBinario.size() -1;
+        while (izquierda<=derecha){
+            int numMedio=(derecha+izquierda)/2;
+            if(targetTrackingNameT.equals(listaBuscarPorTrackingBinario.get(numMedio).nombrePlato)){
+                return listaBuscarPorTrackingBinario.get(numMedio);
+            }else if(listaBuscarPorTrackingBinario.get(numMedio).getNombrePlato().compareTo(targetTrackingNameT) < 0){
+                izquierda=numMedio+1;
+            }else{
+                derecha=numMedio-1;
+            }
+        }
+        return null;
+    }
+    public void BurbujaxNombre(){
+        arregloBurbuja = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloBurbuja.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloBurbuja.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arregloBurbuja.get(j).getNombrePlato().compareTo(arregloBurbuja.get(j+1).getNombrePlato()) >0 ) {
+                    Plato temp = arregloBurbuja.get(j);
+                    arregloBurbuja.set(j,arregloBurbuja.get(j+1)) ;
+                    arregloBurbuja.set(j+1,temp);
+                }
+            }
+        }
+    }
+    public void BurbujaA(){
+        int n = platosMenu.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (platosMenu.get(j).getNombrePlato().compareTo(platosMenu.get(j+1).getNombrePlato()) > 0 ) {
+                    Plato temp = platosMenu.get(j);
+                    platosMenu.set(j,platosMenu.get(j+1)) ;
+                    platosMenu.set(j+1,temp);
+                }
+            }
+        }
+    }
+
+    public void BurbujaxCalorias(){
+        arregloBurbuja = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloBurbuja.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloBurbuja.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arregloBurbuja.get(j).getCalorias() > arregloBurbuja.get(j+1).getCalorias()) {
+                    Plato temp = arregloBurbuja.get(j);
+                    arregloBurbuja.set(j,arregloBurbuja.get(j+1)) ;
+                    arregloBurbuja.set(j+1,temp);
+                }
+            }
+        }
+    }
+    public void BurbujaxTiempo(){
+        arregloBurbuja = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloBurbuja.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloBurbuja.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arregloBurbuja.get(j).getTimepoPreparacion() > arregloBurbuja.get(j+1).getTimepoPreparacion()) {
+                    Plato temp = arregloBurbuja.get(j);
+                    arregloBurbuja.set(j,arregloBurbuja.get(j+1)) ;
+                    arregloBurbuja.set(j+1,temp);
+                }
+            }
+        }
+    }
+    /*public void Burbuja(){
+        int n = platosMenu.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (platosMenu.get(j).getCalorias() > platosMenu.get(j+1).getCalorias()  ) {
+                    double temp = platosMenu.get(j).getCalorias();
+                    platosMenu.get(j).setCalorias(platosMenu.get(j+1).getCalorias()); ;
+                    platosMenu.get(j+1).setCalorias(temp);
+                }
+            }
+        }
+    }*/
+    public void BurbujaxPrecio(){
+        arregloBurbuja = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloBurbuja.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloBurbuja.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arregloBurbuja.get(j).getPrecioPlato() > arregloBurbuja.get(j+1).getPrecioPlato()  ) {
+                    Plato temp = arregloBurbuja.get(j);
+                    arregloBurbuja.set(j,arregloBurbuja.get(j+1)) ;
+                    arregloBurbuja.set(j+1,temp);
+                }
+            }
+        }
+    }
+    public void Insercion(){
+        arregloInsercion = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloInsercion.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloInsercion.size();
+        for (int i = 1; i < n; ++i) {
+            Plato key = arregloInsercion.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && arregloInsercion.get(j).getNombrePlato().compareTo(key.getNombrePlato()) > 0) {
+                arregloInsercion.set(j+1,arregloInsercion.get(j));
+                j = j - 1;
+            }
+            arregloInsercion.set(j+1,key);
+        }
+    }
+
+    public List<Plato> getArregloInsercion() {
+        return arregloInsercion;
+    }
+
+    /*public void BurbujaPrecio(){
+        int n = platosMenu.size();
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (platosMenu.get(j).getPrecioPlato() > platosMenu.get(j+1).getPrecioPlato()  ) {
+                    double temp = platosMenu.get(j).getPrecioPlato();
+                    platosMenu.get(j).setPrecioPlato(platosMenu.get(j+1).getPrecioPlato()); ;
+                    platosMenu.get(j+1).setPrecioPlato(temp);
+                }
+            }
+        }
+    }*/
+
+    public List<Plato> getPlatosMenu() {
+        return platosMenu;
+    }
+
+    public List<Plato> getArregloBurbuja() {
+        return arregloBurbuja;
+    }
+
+    /*public void Insercion(){
+        arregloInsercion = new ArrayList<>();
+        for (Plato packagesRetornar:platosMenu){
+            try {
+                Plato packageAux=packagesRetornar.deepClone();
+                arregloInsercion.add(packageAux);
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        int n = arregloInsercion.size();
+        for (int i = 1; i < n; ++i) {
+            String key = arregloInsercion.get(i).getNombrePlato();
+            int j = i - 1;
+
+            while (j >= 0 && arregloInsercion.get(j).getNombrePlato().compareTo(key) > 0) {
+                arregloInsercion.get(j+1).setNombrePlato(arregloInsercion.get(j).getNombrePlato());
+                j = j - 1;
+            }
+            arregloInsercion.get(j+1).setNombrePlato(key) ;
+        }
+    }*/
+
+    /*public List<Plato> getArregloInsercion() {
+        return arregloInsercion;
+    }*/
+
+}
---
+++ ExamenProgra2FINALCorregido/ExamenProgreso2ProgramacionIII/src/mainForm.java
@@ -0,0 +1,229 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mainForm extends JFrame {
+
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane1;
+    private JTextField textIngresoNombre;
+    private JButton ingresarPlatoButton;
+    private JTextArea textAIngresoPlatos;
+    private JButton QuemarDatosButton;
+    private JTextField textIngresoPrecio;
+    private JTextField textIngresoCalorias;
+    private JTextField textIngresoPreparacion;
+    private JButton buscarModifButton;
+    private JButton modificarModifButton;
+    private JTextField textoModifNombre;
+    private JTextField textoModifPrecio;
+    private JTextField textoModifCalorias;
+    private JTextField textoModifPreparacion;
+    private JTextArea textAModif;
+    private JButton ButtonBuscarEliminar;
+    private JTextField textNombreEliminar;
+    private JTextArea textAEliminar;
+    private JButton eliminarButton;
+    private JComboBox comboBoxOrder;
+    private JButton mostrarButton;
+    private JTextArea textAMostrar;
+    private JButton buscarButton;
+    private JTextField textBuscarPlatoOrden;
+    private JTextArea textArea1;
+    private JButton ordenarBurbujaNombreButton;
+    Plato platoNew;
+    Menu menuNew= new Menu();
+    Ordenamiento ordenamientoNew= new Ordenamiento();
+    Plato platoA=new Plato("Papa",2.0,100,30);
+    Plato platoB=new Plato("Pure",2.75,60,50);
+    Plato platoC=new Plato("Lasagna",2.5,40,20);
+    Plato platoD=new Plato("Hongos",2.90,50,10);
+
+    public mainForm() {
+
+        ingresarPlatoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (textIngresoNombre.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "El campo de nombre está vacío");
+                } else if (textIngresoPrecio.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "El campo de precio está vacío");
+                } else if (textIngresoCalorias.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "El campo de calorias está vacío");
+                } else if (Integer.valueOf(textIngresoPreparacion.getText())<0) {
+                    JOptionPane.showMessageDialog(null, "El campo del calorias debe ser positivo");
+                } else {
+                    String nombrePlato=textIngresoNombre.getText();
+                    double precioPlato=Double.valueOf(textIngresoPrecio.getText());
+                    double calorias=Double.valueOf(textIngresoCalorias.getText());
+                    int timepoPreparacion=Integer.valueOf(textIngresoPreparacion.getText());
+                    //if (menuNew.searchByTrackingNameLineal(menuNew.platosMenu, nombrePlato) == -1) {
+                    if (menuNew.searchBinary(menuNew.platosMenu, textIngresoNombre.getText()) == null) {
+                        if (!textIngresoNombre.getText().isEmpty() && !textIngresoPrecio.getText().isEmpty() && !textIngresoCalorias.getText().isEmpty()&& !textIngresoPreparacion.getText().isEmpty()) {
+                            platoNew = new Plato(nombrePlato,precioPlato,calorias,timepoPreparacion);
+                            menuNew.addPlatos(platoNew);
+                            JOptionPane.showMessageDialog(null, "Se ha creado con éxito el plato");
+                            menuNew.BurbujaA();
+                            System.out.println(menuNew.getPlatosMenu().toString());
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Ya existe un plato con ese nombre ");
+                    }
+                }
+
+
+            }
+        });
+        QuemarDatosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuNew.platosMenu.add(platoA);
+                menuNew.platosMenu.add(platoB);
+                menuNew.platosMenu.add(platoC);
+                menuNew.platosMenu.add(platoD);
+                menuNew.BurbujaA();
+                for (int i=0;i<menuNew.platosMenu.size();i++){
+                    textAIngresoPlatos.append(menuNew.platosMenu.get(i) + "\n");
+                }
+            }
+        });
+        buscarModifButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textoModifNombre.getText().isEmpty() && menuNew.searchBinary(menuNew.platosMenu,textoModifNombre.getText()) !=null) {
+
+                    activarModificar();
+                    Plato platoBuscar=menuNew.searchBinary(menuNew.platosMenu,textoModifNombre.getText());
+                    textoModifPrecio.setText(String.valueOf(platoBuscar.getPrecioPlato()));
+                    textoModifCalorias.setText(String.valueOf(platoBuscar.getCalorias()));
+                    textoModifPreparacion.setText(String.valueOf(platoBuscar.getTimepoPreparacion()));
+                }else {
+                    JOptionPane.showMessageDialog(null, "No existe ese plato o el campo esta vacio");
+                }
+            }
+        });
+        modificarModifButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double precioNuevo=Double.parseDouble(textoModifPrecio.getText());
+                double caloriasNuevo=Double.parseDouble(textoModifCalorias.getText());
+                int tiempoPNuevo=Integer.parseInt(textoModifPreparacion.getText());
+                Plato platoAuxiliar=menuNew.searchBinary(menuNew.platosMenu,textoModifNombre.getText());
+                if(platoAuxiliar!=null) {
+                    if (!textoModifPrecio.getText().isEmpty()) {
+                        modificarPrecio(platoAuxiliar,precioNuevo);
+                    }if (!textoModifCalorias.getText().isEmpty()){
+                        modificarCalorias(platoAuxiliar,caloriasNuevo);
+                    }if (!textoModifPreparacion.getText().isEmpty()) {
+                        modificarTiempoPreparacion(platoAuxiliar,tiempoPNuevo);
+                    }
+
+                    textAModif.setText(platoAuxiliar.toString());
+                }else{
+                    JOptionPane.showMessageDialog(null,"Verifique el nombre porfavor");
+                }
+            }
+        });
+        ButtonBuscarEliminar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textNombreEliminar.getText().isEmpty() && menuNew.searchBinary(menuNew.platosMenu,textNombreEliminar.getText()) !=null) {
+
+                    activarModificar();
+                    Plato platoBuscar=menuNew.searchBinary(menuNew.platosMenu,textNombreEliminar.getText());
+                    textAEliminar.setText(" "+platoBuscar.toString());
+                }else {
+                    JOptionPane.showMessageDialog(null, "No existe ese plato o el campo esta vacio");
+                }
+            }
+        });
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                menuNew.removePackage(menuNew.platosMenu,textNombreEliminar.getText());
+                JOptionPane.showMessageDialog(null,"Se ha elimiado correctamente");
+                textAEliminar.setText(" ");
+            }
+        });
+        mostrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (comboBoxOrder.getSelectedIndex()== 0){
+                    //menuNew.BurbujaxNombre();
+                    menuNew.Insercion();
+                    textAMostrar.setText(" ");
+                    for (int i=0;i<menuNew.getArregloInsercion().size();i++){
+                        textAMostrar.append(menuNew.getArregloInsercion().get(i) + "\n");
+                    }
+                }if (comboBoxOrder.getSelectedIndex() ==1){
+                    menuNew.BurbujaxPrecio();
+                    textAMostrar.setText(" ");
+                    for (int i=0;i<menuNew.getArregloBurbuja().size();i++){
+                        textAMostrar.append(menuNew.getArregloBurbuja().get(i) + "\n");
+                    }
+                }if (comboBoxOrder.getSelectedIndex() ==2){
+                    menuNew.BurbujaxCalorias();
+                    textAMostrar.setText(" ");
+                    for (int i=0;i<menuNew.getArregloBurbuja().size();i++){
+                        textAMostrar.append(menuNew.getArregloBurbuja().get(i) + "\n");
+                    }
+                }if (comboBoxOrder.getSelectedIndex() ==3){
+                    menuNew.BurbujaxTiempo();
+                    textAMostrar.setText(" ");
+                    for (int i=0;i<menuNew.getArregloBurbuja().size();i++){
+                        textAMostrar.append(menuNew.getArregloBurbuja().get(i) + "\n");
+                    }
+                }
+
+            }
+        });
+
+        mostrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textBuscarPlatoOrden.getText().isEmpty() && menuNew.searchBinary(menuNew.platosMenu,textBuscarPlatoOrden.getText()) !=null) {
+
+                    Plato platoBuscar=menuNew.searchBinary(menuNew.platosMenu,textBuscarPlatoOrden.getText());
+                    textAMostrar.setText(platoBuscar.toString());
+                }else {
+                    JOptionPane.showMessageDialog(null, "No existe ese plato o el campo esta vacio");
+                }
+            }
+        });
+    }
+
+    //Get mainPanel
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+    public void activarModificar(){
+        textoModifPrecio.setEditable(true);
+        textoModifCalorias.setEditable(true);
+        textoModifPreparacion.setEditable(true);
+    }public void desActivarModificar(){
+        textoModifPrecio.setEditable(false);
+        textoModifCalorias.setEditable(false);
+        textoModifPreparacion.setEditable(false);
+    }
+    public void modificarPrecio(Plato empleadoModificar,double precio){
+        empleadoModificar.setPrecioPlato(precio);
+        JOptionPane.showMessageDialog(null,"Se ha modificado con exito el precio");
+    }
+    public void modificarCalorias(Plato empleadoModificar,double calorias){
+
+        empleadoModificar.setCalorias(calorias);
+        JOptionPane.showMessageDialog(null, "Se ha modificado con exito las calorias");
+
+    }
+    public void modificarTiempoPreparacion(Plato empleadoModificar,int tiempoP){
+
+        empleadoModificar.setTimepoPreparacion(tiempoP);
+        JOptionPane.showMessageDialog(null,"Se ha modificado con exito el tiempo");
+    }
+}
